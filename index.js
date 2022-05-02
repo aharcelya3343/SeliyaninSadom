833dedc8feb0dfed137c498a96fd946d3c4eb094
aharcelya3343 <104795878+aharcelya3343@users.noreply.github.com>
2022-05-03 01:49:29 +0500

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,25 @@
+<style type="text/css">
+    li {
+        font: 18px serif;
+    }
+    q:lang(de) {
+        quotes: "\201E" "\201C"; /* Немецкие кавычки */
+    }
+    q:lang(en) {
+        quotes: "\201C" "\201D"; /* Английские кавычки */
+    }
+    q:lang(fr) {
+        quotes: "\00AB" "\00BB"; /* Французские кавычки */
+    }
+</style>
+<ul>
+    <li>Цитата на немецком языке:
+        <q lang="de">In Hoffnung schweben, macht süß das Leben</q>.
+    </li>
+    <li>Цитата на английском языке:
+        <q lang="en">То be or not to be</q>.
+    </li>
+    <li>Цитата на французском языке:
+        <q lang="fr">Dans chaque malheur – cherchez la femme</q>.
+    </li>
+</ul>
